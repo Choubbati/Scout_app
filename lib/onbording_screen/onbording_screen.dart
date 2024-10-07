f10daae4048526908bcf4fc28030291e84d26a7c
chouaib <choubbati@gmail.com>
2024-10-07 18:46:27 +0100

    create onbording with animate transition

--- lib/onbording_screen/onbording_screen.dart
+++ lib/onbording_screen/onbording_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:scout_app/constante.dart';
 import 'package:scout_app/onbording_screen/first_screen.dart';
 import 'package:scout_app/onbording_screen/second_screen.dart';
 import 'package:scout_app/onbording_screen/thired_screen.dart';
@@ -13,24 +16,34 @@ class OnbordingScreen extends StatefulWidget {
 }
 
 class _OnbordingScreenState extends State<OnbordingScreen> {
-  PageController controller = PageController();
   int index = 0;
+  bool isOut = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
         children: [
-          Expanded(
-            child: PageView(
-              onPageChanged: (value) {
-                setState(() {
-                  index = value;
-                });
-              },
-              controller: controller,
-              children: const [FirstScreen(), SecondScreen(), ThiredScreen()],
+          SizedBox(
+            width: width(context),
+            height: height(context) * .5,
+            child: AnimatedScale(
+              scale: isOut ? 0 : 1,
+              duration: Duration(milliseconds: 250),
+              child: Image.asset(images[index]),
             ),
           ),
+          Expanded(
+              child: Stack(
+            children: [
+              
+              AnimatedPositioned(
+                left: isOut?width(context)+100:width(context)*.28,
+                child: Text(title[index],textAlign: TextAlign.center,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: const Color.fromARGB(255, 110, 1, 152)),),
+                duration: Duration(milliseconds: 250),
+              )
+            ],
+          )),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -50,18 +63,37 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                InkWell(child: Text(index==2?"Register":"Skip",style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)),
                 InkWell(
-                  onTap: () {
-                    controller.animateToPage(index + 1, duration: const Duration(milliseconds: 250), curve: Curves.linear);
-                  },
+                    child: Text(
+                  index == 2 ? "Register" : "Skip",
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                )),
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        isOut = !isOut;
+                      });
+                      Timer(Duration(milliseconds: 300), () {
+                        index = index > 1 ? 0 : index + 1;
+                        setState(() {
+                          isOut = !isOut;
+                        });
+                      });
+                    },
                     child: Container(
                         padding: const EdgeInsets.all(12),
                         decoration: BoxDecoration(
-                         color: const Color.fromARGB(255, 6, 108, 10),
+                          color: const Color.fromARGB(255, 110, 1, 152),
                           borderRadius: BorderRadius.circular(30),
                         ),
-                        child: Text(index==2?"Login":"Next",style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.white),)))
+                        child: Text(
+                          index == 2 ? "Login" : "Next",
+                          style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )))
               ],
             ),
           )
@@ -81,9 +113,21 @@ class CostumIndicator extends StatelessWidget {
       duration: const Duration(milliseconds: 250),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(100),
-          color: active ? const Color.fromARGB(255, 6, 108, 10) : Colors.grey),
+          color: active ? Color.fromARGB(255, 110, 1, 152) : Colors.grey),
       width: active ? 30 : 10,
       height: 10,
     );
   }
 }
+
+List images = [
+  "lib/onbording_screen/assets/images/logo.png",
+  "lib/onbording_screen/assets/images/MAJMO3A.png",
+  "lib/onbording_screen/assets/images/bnowalid.png"
+];
+
+List title = [
+  "No ads while \nlistening music",
+  "Listen to \nmusic offline",
+  "      share \nYour playlist"
+];
